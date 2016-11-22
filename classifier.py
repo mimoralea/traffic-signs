cd49a9cf26e47796af75aa7c2f7576919a1a49a4
Miguel Morales <mimoralea@gmail.com>
2016-11-22 16:33:44 -0600

    Initial model after adding dropout and images to tensorboard

---
+++ classifier.py
@@ -0,0 +1,469 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+
+import pickle
+
+import tensorflow as tf
+import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
+
+import time
+import os
+
+NUM_CLASSES = 43
+IMAGE_SIZE = 32
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
+
+# Constants describing the training process.
+MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
+NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
+LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
+INITIAL_LEARNING_RATE = 0.05       # Initial learning rate.
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_integer('max_steps', 1000000,
+                            """Number of batches to run.""")
+tf.app.flags.DEFINE_integer('batch_size', 128,
+                            """Number of images to process in a batch.""")
+tf.app.flags.DEFINE_boolean('log_device_placement', False,
+                            """Whether to log device placement.""")
+tf.app.flags.DEFINE_string('train_dir', 'train_dir',
+                           """Directory where to write event logs """
+                           """and checkpoint.""")
+tf.app.flags.DEFINE_string('checkpoint_dir', 'checkpoint_dir',
+                           """Directory where to write event logs """
+                           """and checkpoint.""")
+tf.app.flags.DEFINE_string('eval_dir', 'eval_dir',
+                           """Direcotry where to write event logs.""")
+tf.app.flags.DEFINE_integer('eval_interval_secs', 60 * 5,
+                            """How often to run the eval.""")
+tf.app.flags.DEFINE_integer('num_examples', 10000,
+                            """Number of examples to run.""")
+
+def _add_loss_summaries(total_loss):
+    """Add summaries for losses in CIFAR-10 model.
+    Generates moving average for all losses and associated summaries for
+    visualizing the performance of the network.
+    Args:
+      total_loss: Total loss from loss().
+    Returns:
+      loss_averages_op: op for generating moving averages of losses.
+    """
+    # Compute the moving average of all individual losses and the total loss.
+    loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+    losses = tf.get_collection('losses')
+    loss_averages_op = loss_averages.apply(losses + [total_loss])
+
+    # Attach a scalar summary to all individual losses and the total loss; do the
+    # same for the averaged version of the losses.
+    for l in losses + [total_loss]:
+        # Name each loss as '(raw)' and name the moving average version of the loss
+        # as the original loss name.
+        tf.scalar_summary(l.op.name +' (raw)', l)
+        tf.scalar_summary(l.op.name, loss_averages.average(l))
+
+    return loss_averages_op
+
+
+def loss_func(logits, labels):
+    """Add L2Loss to all the trainable variables.
+    Add summary for "Loss" and "Loss/avg".
+    Args:
+      logits: Logits from inference().
+      labels: Labels from distorted_inputs or inputs(). 1-D tensor
+              of shape [batch_size]
+    Returns:
+      Loss tensor of type float.
+    """
+    # Calculate the average cross entropy loss across the batch.
+    labels = tf.cast(labels, tf.int64)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits, labels, name='cross_entropy_per_example')
+    cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+    tf.add_to_collection('losses', cross_entropy_mean)
+
+    # The total loss is defined as the cross entropy loss plus all of the weight
+    # decay terms (L2 loss).
+    return tf.add_n(tf.get_collection('losses'), name='total_loss')
+
+def _activation_summary(x):
+    """Helper to create summaries for activations.
+    Creates a summary that provides a histogram of activations.
+    Creates a summary that measures the sparsity of activations.
+    Args:
+        x: Tensor
+    Returns:
+        nothing
+    """
+    tf.histogram_summary(x.op.name + '/activations', x)
+    tf.scalar_summary(x.op.name + '/sparsity', tf.nn.zero_fraction(x))
+
+def _variable_on_cpu(name, shape, initializer):
+    """Helper to create a Variable stored on CPU memory.
+    Args:
+        name: name of the variable
+        shape: list of ints
+        initializer: initializer for Variable
+    Returns:
+        Variable Tensor
+    """
+    with tf.device('/cpu:0'):
+        var = tf.get_variable(name, shape, initializer=initializer, dtype=tf.float32)
+    return var
+
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+    """Helper to create an initialized Variable with weight decay.
+    Note that the Variable is initialized with a truncated normal distribution.
+    A weight decay is added only if one is specified.
+    Args:
+        name: name of the variable
+        shape: list of ints
+        stddev: standard deviation of a truncated Gaussian
+        wd: add L2Loss weight decay multiplied by this float. If None, weight
+            decay is not added for this Variable.
+    Returns:
+        Variable Tensor
+    """
+    var = _variable_on_cpu(
+            name,
+            shape,
+            tf.truncated_normal_initializer(stddev=stddev, dtype=tf.float32))
+    if wd is not None:
+        weight_decay = tf.mul(tf.nn.l2_loss(var), wd, name='weight_loss')
+        tf.add_to_collection('losses', weight_decay)
+    return var
+
+
+def put_kernels_on_grid (kernel, grid_Y, grid_X, pad = 1):
+
+    '''Visualize conv. features as an image (mostly for the 1st layer).
+    Place kernel into a grid, with some paddings between adjacent filters.
+
+    Args:
+      kernel:            tensor of shape [Y, X, NumChannels, NumKernels]
+      (grid_Y, grid_X):  shape of the grid. Require: NumKernels == grid_Y * grid_X
+                           User is responsible of how to break into two multiples.
+      pad:               number of black pixels around each filter (between them)
+
+    Return:
+      Tensor of shape [(Y+2*pad)*grid_Y, (X+2*pad)*grid_X, NumChannels, 1].
+    '''
+
+    x_min = tf.reduce_min(kernel)
+    x_max = tf.reduce_max(kernel)
+
+    kernel1 = (kernel - x_min) / (x_max - x_min)
+
+    # pad X and Y
+    x1 = tf.pad(kernel1, tf.constant( [[pad,pad],[pad, pad],[0,0],[0,0]] ), mode = 'CONSTANT')
+
+    # X and Y dimensions, w.r.t. padding
+    Y = kernel1.get_shape()[0] + 2 * pad
+    X = kernel1.get_shape()[1] + 2 * pad
+
+    channels = kernel1.get_shape()[2]
+
+    # put NumKernels to the 1st dimension
+    x2 = tf.transpose(x1, (3, 0, 1, 2))
+    # organize grid on Y axis
+    x3 = tf.reshape(x2, tf.pack([grid_X, Y * grid_Y, X, channels])) #3
+
+    # switch X and Y axes
+    x4 = tf.transpose(x3, (0, 2, 1, 3))
+    # organize grid on X axis
+    x5 = tf.reshape(x4, tf.pack([1, X * grid_X, Y * grid_Y, channels])) #3
+
+    # back to normal order (not combining with the next step for clarity)
+    x6 = tf.transpose(x5, (2, 1, 3, 0))
+
+    # to tf.image_summary order [batch_size, height, width, channels],
+    #   where in this case batch_size == 1
+    x7 = tf.transpose(x6, (3, 0, 1, 2))
+
+    # scale to [0, 255] and convert to uint8
+    return tf.image.convert_image_dtype(x7, dtype = tf.uint8)
+
+
+def inference(images):
+    """Build the CIFAR-10 model.
+    Args:
+      images: Images returned from distorted_inputs() or inputs().
+    Returns:
+      Logits.
+    """
+    # We instantiate all variables using tf.get_variable() instead of
+    # tf.Variable() in order to share variables across multiple GPU training runs.
+    # If we only ran this model on a single GPU, we could simplify this function
+    # by replacing all instances of tf.get_variable() with tf.Variable().
+    #
+    # conv1
+
+    print("images", images.get_shape())
+    with tf.variable_scope('conv1') as scope:
+        kernel = _variable_with_weight_decay('weights',
+                                             shape=[5, 5, 3, 64],
+                                             stddev=5e-2,
+                                             wd=0.0)
+        conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
+        bias = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(bias, name=scope.name)
+        _activation_summary(conv1)
+
+        grid_x = grid_y = 8
+        grid = put_kernels_on_grid (kernel, (grid_y, grid_x))
+        tf.image_summary(scope.name + '/features', grid, max_images=1)
+
+
+    print("conv1", conv1.get_shape())
+
+    # pool1
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+                           padding='SAME', name='pool1')
+
+    print("pool1", pool1.get_shape())
+
+    # norm1
+    norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                      name='norm1')
+
+    print("norm1", norm1.get_shape())
+
+    # conv2
+    with tf.variable_scope('conv2') as scope:
+        kernel = _variable_with_weight_decay('weights',
+                                             shape=[5, 5, 64, 64],
+                                             stddev=5e-2,
+                                             wd=0.0)
+        conv = tf.nn.conv2d(norm1, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
+        bias = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(bias, name=scope.name)
+        _activation_summary(conv2)
+
+        grid_x = grid_y = 8
+        grid = put_kernels_on_grid (kernel, (grid_y, grid_x))
+        tf.image_summary(scope.name + '/features', grid, max_images=1)
+
+    print("conv2", conv2.get_shape())
+
+    # norm2
+    norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                      name='norm2')
+
+    print("norm2", norm2.get_shape())
+
+    # pool2
+    pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
+                           strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+
+    print("pool2", pool2.get_shape())
+
+    # local3
+    with tf.variable_scope('local3') as scope:
+        # Move everything into depth so we can perform a single matrix multiply.
+        dim = np.prod(pool2.get_shape().as_list()[1:])
+        reshape = tf.reshape(pool2, [-1, dim])
+        print("reshape", reshape.get_shape())
+        weights = _variable_with_weight_decay('weights', shape=[dim, 384],
+                                              stddev=0.04, wd=0.004)
+        biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+        local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+        _activation_summary(local3)
+
+        grid_x = grid_y = 8
+        grid = put_kernels_on_grid (weights, (grid_y, grid_x))
+        tf.image_summary(scope.name + '/features', grid, max_images=1)
+
+    # local4
+    with tf.variable_scope('local4') as scope:
+        weights = _variable_with_weight_decay('weights', shape=[384, 192],
+                                              stddev=0.04, wd=0.004)
+        biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
+        local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+        _activation_summary(local4)
+
+        grid_x = grid_y = 8
+        grid = put_kernels_on_grid (weights, (grid_y, grid_x))
+        tf.image_summary(scope.name + '/features', grid, max_images=1)
+
+
+    keep_prob = tf.placeholder(tf.float32)
+    local4_drop = tf.nn.dropout(local4, keep_prob)
+
+    # softmax, i.e. softmax(WX + b)
+    with tf.variable_scope('softmax_linear') as scope:
+        weights = _variable_with_weight_decay('weights', [192, NUM_CLASSES],
+                                              stddev=1/192.0, wd=0.0)
+        biases = _variable_on_cpu('biases', [NUM_CLASSES],
+                                  tf.constant_initializer(0.0))
+        softmax_linear = tf.add(tf.matmul(local4_drop, weights), biases, name=scope.name)
+        _activation_summary(softmax_linear)
+
+        grid_x = grid_y = 8
+        grid = put_kernels_on_grid (weights, (grid_y, grid_x))
+        tf.image_summary(scope.name + '/features', grid, max_images=1)
+
+    softmax_normalized = tf.nn.softmax(softmax_linear)        
+    return softmax_normalized
+
+
+def train_func(total_loss, global_step):
+    """Train CIFAR-10 model.
+    Create an optimizer and apply to all trainable variables. Add moving
+    average for all trainable variables.
+    Args:
+      total_loss: Total loss from loss().
+      global_step: Integer Variable counting the number of training steps
+        processed.
+    Returns:
+      train_op: op for training.
+    """
+    # Variables that affect learning rate.
+    num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / FLAGS.batch_size
+    decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
+
+    # Decay the learning rate exponentially based on the number of steps.
+    lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
+                                    global_step,
+                                    decay_steps,
+                                    LEARNING_RATE_DECAY_FACTOR,
+                                    staircase=True)
+    tf.scalar_summary('learning_rate', lr)
+
+    # Generate moving averages of all losses and associated summaries.
+    loss_averages_op = _add_loss_summaries(total_loss)
+
+    # Compute gradients.
+    with tf.control_dependencies([loss_averages_op]):
+        opt = tf.train.GradientDescentOptimizer(lr)
+        grads = opt.compute_gradients(total_loss)
+
+    # Apply gradients.
+    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+    # Add histograms for trainable variables.
+    for var in tf.trainable_variables():
+        tf.histogram_summary(var.op.name, var)
+
+    # Add histograms for gradients.
+    for grad, var in grads:
+        if grad is not None:
+            tf.histogram_summary(var.op.name + '/gradients', grad)
+
+    # Track the moving averages of all trainable variables.
+    variable_averages = tf.train.ExponentialMovingAverage(
+        MOVING_AVERAGE_DECAY, global_step)
+    variables_averages_op = variable_averages.apply(tf.trainable_variables())
+
+    with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
+        train_op = tf.no_op(name='train')
+
+    return train_op
+
+
+def main(argv=None):  # pylint: disable=unused-argument
+
+    training_file = 'data/train.p'
+    testing_file = 'data/test.p'
+
+    with open(training_file, mode='rb') as f:
+        train = pickle.load(f)
+    with open(testing_file, mode='rb') as f:
+        test = pickle.load(f)
+
+    X_train, y_train = train['features'], train['labels']
+    X_test, y_test = test['features'], test['labels']
+
+    n_train = X_train.shape[0]
+    n_test = X_test.shape[0]
+
+    image_shape = X_train[0].shape
+    n_classes = len(np.unique(y_train))
+
+    img_width = image_shape[0]
+    img_height = image_shape[1]
+    img_channels = image_shape[2]
+
+    print("Number of training examples =", n_train)
+    print("Number of testing examples =", n_test)
+    print("Image data shape =", image_shape)
+    print("Number of classes =", n_classes)
+
+
+    with tf.Graph().as_default():
+        global_step = tf.Variable(0, trainable=False)
+
+        # Get images and labels
+        images = tf.placeholder(tf.float32, shape=[None, img_width, img_height, img_channels])
+        labels = tf.placeholder(tf.float32, shape=[None])
+
+        # Manipulate the images in the batch randomly to artificially create
+        # more data and make the network able to generalize better
+        images = tf.map_fn(lambda img: tf.image.random_flip_left_right(img), images) 
+        images = tf.map_fn(lambda img: tf.image.random_brightness(img, max_delta=63), images)
+        images = tf.map_fn(lambda img: tf.image.random_contrast(img, lower=0.2, upper=1.8), images)
+        images = tf.map_fn(lambda img: tf.image.per_image_whitening(img), images)
+
+        # Calculate the logits and loss
+        logits = inference(images)
+        loss = loss_func(logits, labels)
+
+        # Create a training operation that updates the
+        # network parameters
+        train_op = train_func(loss, global_step)
+
+        # Prepare to save and collect summary data
+        saver = tf.train.Saver(tf.all_variables())
+        summary_op = tf.merge_all_summaries()
+
+        # Build an initialization operation to run below.
+        init = tf.initialize_all_variables()
+
+        # Start running operations on the Graph.
+        sess = tf.Session(config=tf.ConfigProto(
+            log_device_placement=FLAGS.log_device_placement))
+        sess.run(init)
+
+        # Start the queue runners.
+        tf.train.start_queue_runners(sess=sess)
+
+        summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, sess.graph)
+
+        for step in xrange(FLAGS.max_steps):
+            start_time = time.time()
+            idx = np.random.randint(len(X_train), size=FLAGS.batch_size)
+
+            sess.run(train_op, feed_dict={images:X_train[idx], labels:y_train[idx], keep_prob:0.4})
+            loss_value = sess.run(loss, feed_dict={images:X_train[idx], labels:y_train[idx], keep_prob:1.0})
+
+            duration = time.time() - start_time
+
+            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+            if step % 10 == 0:
+                num_examples_per_step = FLAGS.batch_size
+                examples_per_sec = num_examples_per_step / duration
+                sec_per_batch = float(duration)
+
+                format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                              'sec/batch)')
+                print (format_str % (datetime.now(), step, loss_value,
+                                   examples_per_sec, sec_per_batch))
+
+            if step % 100 == 0:
+                summary_str = sess.run(summary_op, feed_dict={images:X_train[idx], labels:y_train[idx], keep_prob:1.0})
+                summary_writer.add_summary(summary_str, step)
+
+            # Save the model checkpoint periodically.
+            if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+                checkpoint_path = os.path.join(FLAGS.checkpoint_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_path, global_step=step)
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ evaluation.py
@@ -0,0 +1,154 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import math
+import time
+import pickle
+
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import classifier
+
+FLAGS = tf.app.flags.FLAGS
+
+def eval_once(saver, summary_writer, top_k_op,
+              summary_op, X_test, y_test, images, labels):
+    """Run Eval once.
+    Args:
+      saver: Saver.
+      summary_writer: Summary writer.
+      top_k_op: Top K op.
+      summary_op: Summary op.
+    """
+    with tf.Session() as sess:
+        # Restores from checkpoint
+        ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+        else:
+            print('No checkpoint file found')
+            return
+
+        predictions = sess.run(top_k_op, feed_dict={images:X_test, labels:y_test, keep_prob:1.0})
+        print(predictions)
+        true_count = np.sum(predictions)
+        precision = true_count / len(X_test)
+        summary = tf.Summary()
+        summary.ParseFromString(sess.run(summary_op, feed_dict={images:X_test, labels:y_test, keep_prob:1.0}))
+        summary.value.add(tag='Test Precision', simple_value=precision)
+        summary_writer.add_summary(summary, global_step)
+
+    return precision
+
+def eval_img(saver, logits, img, label, images, labels):
+    """Run Eval once.
+    Args:
+      saver: Saver.
+      summary_writer: Summary writer.
+      top_k_op: Top K op.
+      summary_op: Summary op.
+    """
+    with tf.Session() as sess:
+        # Restores from checkpoint
+        ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+        else:
+            print('No checkpoint file found')
+            return
+        sign_strings = pd.read_csv('signnames.csv', index_col=0)
+        probs = sess.run(logits, feed_dict={images:[img], labels:[label], keep_prob:1.0})[0]
+        sign_strings['prob'] = pd.Series(probs, index=sign_strings.index)
+        sign_strings['correct'] = pd.Series(sign_strings.index.values == label, index=sign_strings.index)
+        return sign_strings.sort_values(['prob', 'SignName'], ascending=[0, 1])
+
+def evaluate(X_test, y_test):
+
+    """Eval CIFAR-10 for a number of steps."""
+    with tf.Graph().as_default() as g:
+
+        image_shape = X_test[0].shape
+
+        img_width = image_shape[0]
+        img_height = image_shape[1]
+        img_channels = image_shape[2]
+
+        # Get images and labels for CIFAR-10.
+        images = tf.placeholder(tf.float32, shape=[None, img_width, img_height, img_channels])
+        labels = tf.placeholder(tf.int32, shape=[None])
+
+        # Build a Graph that computes the logits predictions from the
+        # inference model.
+        logits = classifier.inference(images)
+
+        # Calculate predictions.
+        top_k_op = tf.nn.in_top_k(logits, labels, 5)
+
+        # Restore the moving average version of the learned variables for eval.
+        variable_averages = tf.train.ExponentialMovingAverage(
+            classifier.MOVING_AVERAGE_DECAY)
+        variables_to_restore = variable_averages.variables_to_restore()
+        saver = tf.train.Saver(variables_to_restore)
+
+        # Build the summary operation based on the TF collection of Summaries.
+        summary_op = tf.merge_all_summaries()
+
+        summary_writer = tf.train.SummaryWriter(FLAGS.eval_dir, g)
+
+        #precision = eval_once(saver, summary_writer, top_k_op, summary_op,
+        #                      X_test, y_test, images, labels)
+        #print('%s: test set precision %.3f' % (datetime.now(), precision))
+
+        idx = 123
+        img = X_test[idx]
+        label = y_test[idx]
+        predictions = eval_img(saver, logits,
+                               img, label,
+                               images, labels)
+        print(predictions)
+        plt.style.use('ggplot')
+
+        plt.imshow(img)
+        plt.show()
+
+
+def main(argv=None):  # pylint: disable=unused-argument
+
+    training_file = 'data/train.p'
+    testing_file = 'data/test.p'
+
+    with open(training_file, mode='rb') as f:
+        train = pickle.load(f)
+    with open(testing_file, mode='rb') as f:
+        test = pickle.load(f)
+
+    X_train, y_train = train['features'], train['labels']
+    X_test, y_test = test['features'], test['labels']
+
+    n_train = X_train.shape[0]
+    n_test = X_test.shape[0]
+
+    image_shape = X_train[0].shape
+    n_classes = len(np.unique(y_train))
+
+    img_width = image_shape[0]
+    img_height = image_shape[1]
+    img_channels = image_shape[2]
+
+    print("Number of training examples =", n_train)
+    print("Number of testing examples =", n_test)
+    print("Image data shape =", image_shape)
+    print("Number of classes =", n_classes)
+
+    evaluate(X_test, y_test)
+
+
+if __name__ == '__main__':
+    tf.app.run()
